03584406fe7fb3e0dee04c535c34c2a83afcaf38
lebk <terry.lei@symantec.com>
2013-04-17 10:09:15 +0800

    The initial version

---
+++ src/main/java/com/bo/WicketFreeGuilde/HomePage.java
@@ -0,0 +1,18 @@
+package com.bo.WicketFreeGuilde;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		add(new Label("helloMessage", "Hello World"));
+
+		// TODO Add your page's components here
+
+    }
+}
